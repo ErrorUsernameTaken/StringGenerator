d0c43df00b4d0f98683d2286d73d0ded2baf3323
ErrorUsernameTaken <tsteller03@icloud.com>
2018-03-12 16:17:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-
 function makeString() {
 	var string = "";
-	var all2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	var all = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-	for (var i = 0; i < Math.floor(Math.random() * 50); i++) {
+	for (var i = 0; i <  Math.floor(Math.random() * 100); i++) 
 		string += all.charAt(Math.floor(Math.random() * all.length));
 		return string;
-	}
+	
 }
 
 console.log(makeString());
